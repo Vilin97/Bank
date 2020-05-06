92c469cfa1f4e80c833f905e9a5cfc656dfd9180
Vilin97 <vasilin97@gmail.com>
2020-05-06 17:58:36 -0400

    .

--- src/bank/gui/ManagerGUI.java
+++ src/bank/gui/ManagerGUI.java
@@ -8,7 +8,10 @@ import java.util.Currency;
 public class ManagerGUI {
 
     public static void main(String[] args) {
-        JSONTools.readBank();
+        Customers<Customer> customers = new Customers<>();
+        Customer customer1 = new Customer("Vas", "v", "123");
+        Customer customer2 = new Customer("Adam", "a", "123");
+        customer1.openCheckingAccount("acc1", Currency.getInstance("USD"));
         new MainManagerFrame();
     }
 
